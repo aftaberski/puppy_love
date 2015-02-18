a0dc1c3b6463c1ff9668e0caa990fd21d37fc142
Anna Taberski <aftaberski@gmail.com>
2015-02-18 09:38:22 -0500

    add seeds

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# USERS
+anna = User.create(name: "Anna", password: "anna")
+missy = User.create(name: "Missy", password: "missy")
+
+# ARTICLES
+article = Article.create(title:"Sleepy Puppy", photo: "http://bit.ly/LTGDG3", content: "Puppies get sleepy too", user_id: anna.id)
+
+Article.create(title:"Puppies <3 Kittens", photo: "http://bit.ly/1FsDciM", content: "Cuddles for dayz", user_id: missy.id)
+
+Article.create(title:"High Five!", photo: "http://bit.ly/1DqqqQU", content: "Right on.", user_id: anna.id)
